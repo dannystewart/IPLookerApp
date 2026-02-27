ca08d05d2b413e221e46dbbbedb6b46faf21a25a
Danny Stewart <danny@stewart.cc>
2026-02-27 09:12:33 -0500

    Initial Commit

---
+++ IPLooker/IPLookerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPLookerApp.swift
+//  IPLooker
+//
+//  Created by Danny Stewart on 2/27/26.
+//
+
+import SwiftUI
+
+@main
+struct IPLookerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
